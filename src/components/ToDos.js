07ccc1c49cc317d8ec6a74ac885ef907f700868b
Turan Kutuev <turankutuev@Turans-MacBook-Air.local>
2021-11-02 19:46:07 +0100

    feat: add item button

--- src/components/ToDos.js
+++ src/components/ToDos.js
@@ -4,32 +4,36 @@ export default class ToDos extends React.Component {
 
     state = {
         value: '',
-        todos: []
+        todos: [],
     }
 
     render (){
 
 
-        const { todos, value } = this.state;
+        const { todos, value} = this.state;
+        const check = this.state.check;
 
         return (
+            
             <div>
                 
                     {
                         todos.map(todo => <CheckBoxTaskCard label={todo} tags={[]}></CheckBoxTaskCard>)
                     }
-                    
                 
-                <form onSubmit={
+                {this.state.check?<form onSubmit={
                     (event) => {
                         event.preventDefault();
-                        this.setState({ todos: [...todos, value], value: '' });
+                        this.setState({ todos: [...todos, value], value: '', check: false});
                     }
                 }>
-                <input value={value} type="text" onChange={({target: {value}}) => this.setState({value})} />
+                <input style={{margintop: "10px"}} value={value} type="text" onChange={({target: {value}}) => this.setState({value})} />
                 {/* <button onClick={() => this.setState({todos: [...todos, value], value: ''})}>Submit</button> */}
                 
-                </form>
+                </form>:<button className="add-item" onClick={(val) => this.setState(prevState => ({check: !prevState.check}))}>+ Add item</button>}
+                
+               
+                
                 
             </div>
         );
